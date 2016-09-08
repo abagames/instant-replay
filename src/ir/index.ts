57fa92b3312d57c21eff2329427867a6e6eaf615
ABA Games <cs8k-cyu@asahi-net.or.jp>
2016-09-08 22:17:03 +0900

    Add record and replay functions

--- src/ir/index.ts
+++ src/ir/index.ts
@@ -2,6 +2,42 @@ export let options = {
   frameCount: 180,
 };
 
-let statuses = [];
-let events = [];
+let statusAndEvents = times(options.frameCount, () => null);
+let recordingIndex = 0;
+let replayingIndex = 0;
 
+export function record(status, events) {
+  statusAndEvents[recordingIndex] = { status, events };
+  recordingIndex++;
+  if (recordingIndex >= options.frameCount) {
+    recordingIndex = 0;
+  }
+}
+
+export function startReplay() {
+  replayingIndex = recordingIndex + 1;
+  if (replayingIndex >= options.frameCount || statusAndEvents[replayingIndex] == null) {
+    replayingIndex = 0;
+  }
+  return statusAndEvents[replayingIndex].status;
+}
+
+export function getEvents() {
+  if (replayingIndex === recordingIndex) {
+    return null;
+  }
+  let e = statusAndEvents[replayingIndex].events;
+  replayingIndex++;
+  if (replayingIndex >= options.frameCount) {
+    replayingIndex = 0;
+  }
+  return e;
+}
+
+function times(n, fn) {
+  const v = [];
+  for (let i = 0; i < n; i++) {
+    v.push(fn());
+  }
+  return v;
+}
